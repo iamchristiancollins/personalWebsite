5634e18890ba0d96e944b0f5a76f7677107d8eee
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-09-05 10:11:28 -0500

    add data and git PAT

---
+++ constants/data.js
@@ -0,0 +1,66 @@
+const userData = {
+    githubUsername: "iamchristiancollins",
+    name: "Christian Collins",
+    designation: "Software Developer",
+    avatarUrl: "/avatar.png",
+    email: "iamchristiancollins@gmail.com",
+    phone: "+1 9046873838",
+    address: "Birmingham, AL.",
+    projects: [
+      {
+        title: "PlaceholderTech",
+        link: "https://placeholdertech.in",
+        imgUrl: "/placeholdertech.png",
+      },
+      {
+        title: "Portfolio",
+        link: "https://manuarora.in",
+        imgUrl: "/portfolio.png",
+      },
+    ],
+    about: {
+      title:
+        "I'm a software developer that loves building products and web applications that impact millions of lives",
+      description: [
+        `I am a dedicated software developer with a background in mathematics, currently pursuing a Bachelor of Science in Computer Science at the University of Alabama at Birmingham, maintaining a 4.0 GPA. I am proficient in programming languages including Python, Java, C, Javascript, and Assembly, with experience across Windows, macOS, and Linux platforms.`,
+        `My professional experience includes working as a software developer at DarkTower, where I maintain analytical tools and web scrapers, manage PostgreSQL databases, and contribute to enhancing our in-house website using React. Additionally, I serve as a Teaching Assistant for various courses including Algorithms and Data Structures, Computer Organization and Assembly Language, and Systems Programming, where I assist students in problem-solving and debugging, teach weekly labs, and provide individual tutoring.`,
+        `I've strived to be engaged in the tech community as the president of the Association of Computing Machinery and a member of Women in Tech. I also attend regular AI meetups in Birmingham. My goal is to apply my diverse skills to contribute to innovative software solutions in a dynamic company.`,
+      ],
+      currentProject: "TBD",
+      currentProjectUrl: "TBD.com",
+    },
+    experience: [
+      {
+        title: "Software Developer",
+        company: "DarkTower",
+        year: "2024",
+        companyLink: "https://getdarktower.com/",
+        desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+      },
+      {
+        title: "Teaching Assistant",
+        company: "UAB Department of Computer Science",
+        year: "2022",
+        companyLink: "https://uab.edu",
+        desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+      },
+      {
+        title: "Graduation",
+        company: "UAB",
+        year: "2025",
+        companyLink: "https://uab.edu",
+        desc: "Major in Computer Science with a CGPA of 4.0. Nobody asks this but it's okay.",
+      },
+    ],
+    resumeUrl:"TBD",
+    socialLinks: {
+      instagram: "",
+      twitter: "https://twitter.com/",
+      linkedin: "https://linkedin.com/in/iamchristiancollins",
+      github: "https://github.com/iamchristiancollins",
+      facebook: "https://facebook.com/",
+      peerlist: "https://peerlist.io/",
+    },
+  };
+  
+  export default userData;
