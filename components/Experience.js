e5e01c2b56b6e73eb5273d47a8ff76f23a7d4c0a
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-09-05 18:15:23 -0500

    add cards to exp

--- components/Experience.js
+++ components/Experience.js
@@ -30,9 +30,7 @@ export default function Experience() {
             />
             <div className="flex flex-col">
               <p className="text-md">Software Developer</p>
-              <p className="text-small text-default-500">
-                DarkTower February 2024-Present
-              </p>
+              <p className="text-small text-default-500">DarkTower</p>
               <p className="text-small text-default-500">
                 February 2024-Present
               </p>
@@ -84,6 +82,34 @@ export default function Experience() {
             </Link>
           </CardFooter>
         </Card>
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="UAB logo"
+              height={40}
+              radius="sm"
+              src="https://content.sportslogos.net/logos/35/879/full/1518_uab_blazers-primary-2015.png"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">Student</p>
+              <p className="text-small text-default-500">
+                University of Alabama at Birmingham
+              </p>
+              <p className="text-small text-default-500">August 2022-Present</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>Pursuing a Postbaccalaureate degree in Computer Science.</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link isExternal showAnchorIcon href="https://www.uab.edu">
+              Visit UAB's Website.
+            </Link>
+          </CardFooter>
+        </Card>
       </div>
       <h2 className="text-2xl text-gray-500 dark:text-gray-400 text-center mb-10">
         Previous
@@ -92,34 +118,34 @@ export default function Experience() {
         <Card className="max-w-[400px]">
           <CardHeader className="flex gap-3">
             <Image
-              alt="darktower logo"
+              alt="Creekside logo"
               height={40}
               radius="sm"
-              src="https://media.licdn.com/dms/image/C510BAQGhXx5HbNWrLQ/company-logo_200_200/0/1519861747403?e=2147483647&v=beta&t=1CEMMSI-BXn6cF0gsDqsHNtNZdNdnE-RXvxa6_-wEZo"
+              src="https://1stplacesports.com/wp-content/uploads/2020/09/creekside-symbol-front.jpg"
               width={40}
             />
             <div className="flex flex-col">
-              <p className="text-md">Software Developer</p>
+              <p className="text-md">Mathematics Instructor</p>
               <p className="text-small text-default-500">
-                DarkTower February 2024-Present
+                Creekside High School
               </p>
               <p className="text-small text-default-500">
-                February 2024-Present
+                October 2014-June 2022
               </p>
             </div>
           </CardHeader>
           <Divider />
           <CardBody>
-            <p>
-              Help enhance in-house website features using React as well as work
-              with PostgreSQL databases, and maintaining analytical tools and
-              web scrapers in Docker and GitLab.
-            </p>
+            <p>Taught AP Calculus AB and BC.</p>
           </CardBody>
           <Divider />
           <CardFooter>
-            <Link isExternal showAnchorIcon href="https://www.getdarktower.com">
-              Visit DarkTower's Website.
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://www-chs.stjohns.k12.fl.us/"
+            >
+              Visit Creekside's Website.
             </Link>
           </CardFooter>
         </Card>
