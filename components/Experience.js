df9d7d5a41ee5098b67799a2be44e85ddd3b77dc
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-09-05 17:56:19 -0500

    add more details to experience

--- components/Experience.js
+++ components/Experience.js
@@ -12,6 +12,12 @@ import {
 export default function Experience() {
   return (
     <section className="bg-white dark:bg-gray-800">
+      <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-10 md:text-white dark:text-gray-600 text-center lg:text-left">
+        Experience
+      </h1>
+      <h2 className="text-2xl text-gray-500 dark:text-gray-400 text-center mb-10">
+        Current
+      </h2>
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
         <Card className="max-w-[400px]">
           <CardHeader className="flex gap-3">
@@ -24,8 +30,12 @@ export default function Experience() {
             />
             <div className="flex flex-col">
               <p className="text-md">Software Developer</p>
-              <p className="text-small text-default-500">DarkTower February 2024-Present</p>
-              <p className="text-small text-default-500">February 2024-Present</p>
+              <p className="text-small text-default-500">
+                DarkTower February 2024-Present
+              </p>
+              <p className="text-small text-default-500">
+                February 2024-Present
+              </p>
             </div>
           </CardHeader>
           <Divider />
@@ -38,7 +48,7 @@ export default function Experience() {
           </CardBody>
           <Divider />
           <CardFooter>
-            <Link isExternal showAnchorIcon href="getdarktower.com">
+            <Link isExternal showAnchorIcon href="https://www.getdarktower.com">
               Visit DarkTower's Website.
             </Link>
           </CardFooter>
@@ -54,7 +64,9 @@ export default function Experience() {
             />
             <div className="flex flex-col">
               <p className="text-md">Teaching Assistant</p>
-              <p className="text-small text-default-500">UAB Department of Computer Science</p>
+              <p className="text-small text-default-500">
+                UAB Department of Computer Science
+              </p>
               <p className="text-small text-default-500">August 2023-Present</p>
             </div>
           </CardHeader>
@@ -67,8 +79,47 @@ export default function Experience() {
           </CardBody>
           <Divider />
           <CardFooter>
-            <Link isExternal showAnchorIcon href="uab.edu">
+            <Link isExternal showAnchorIcon href="https://www.uab.edu">
               Visit UAB's Website.
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+      <h2 className="text-2xl text-gray-500 dark:text-gray-400 text-center mb-10">
+        Previous
+      </h2>
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+        <Card className="max-w-[400px]">
+          <CardHeader className="flex gap-3">
+            <Image
+              alt="darktower logo"
+              height={40}
+              radius="sm"
+              src="https://media.licdn.com/dms/image/C510BAQGhXx5HbNWrLQ/company-logo_200_200/0/1519861747403?e=2147483647&v=beta&t=1CEMMSI-BXn6cF0gsDqsHNtNZdNdnE-RXvxa6_-wEZo"
+              width={40}
+            />
+            <div className="flex flex-col">
+              <p className="text-md">Software Developer</p>
+              <p className="text-small text-default-500">
+                DarkTower February 2024-Present
+              </p>
+              <p className="text-small text-default-500">
+                February 2024-Present
+              </p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>
+              Help enhance in-house website features using React as well as work
+              with PostgreSQL databases, and maintaining analytical tools and
+              web scrapers in Docker and GitLab.
+            </p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link isExternal showAnchorIcon href="https://www.getdarktower.com">
+              Visit DarkTower's Website.
             </Link>
           </CardFooter>
         </Card>
