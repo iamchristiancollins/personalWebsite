1e7098610c86e7c9afa1197e18988b0bf8786dff
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-09-16 21:54:42 -0500

    fix text color experience

--- components/Projects.js
+++ components/Projects.js
@@ -3,24 +3,21 @@ import userData from "@constants/data";
 
 export default function Projects() {
   return (
-    <section className="bg-gray-800">
+    <section className="bg-gray-800 text-white">
       <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-10 text-center lg:text-left">
         Projects
       </h1>
-      <div
-        className="max-w-6xl mx-auto grid grid-cols-1 sm:text-white md:text-white"
-        style={{ color: "white" }}
-      >
+      <div className="max-w-6xl mx-auto grid grid-cols-1">
         {userData.projects.map((project, idx) => (
           <div key={idx}>
             <a href={project.link} target="_blank" rel="noreferrer">
               <img
                 src={project.imgUrl}
                 alt={project.title}
-                className="rounded-lg shadow-lg w-full h-[500px] object-cover sm:text-white md:text-white"
+                className="rounded-lg shadow-lg w-full h-[500px] object-cover "
               />
             </a>
-            <h1 className="text-2xl font-bold sm:text-white md:text-white text-center my-5">
+            <h1 className="text-2xl font-bold text-center my-5">
               {project.title}
             </h1>
           </div>
