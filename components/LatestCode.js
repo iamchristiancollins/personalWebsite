46c44b21db2205e97c7c58bf231b1fad957a32a6
iamchristiancollins <105734903+iamchristiancollins@users.noreply.github.com>
2024-10-03 09:08:48 -0500

    fix view github button

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -15,7 +15,7 @@ export default function LatestCode({ repositories }) {
           </h1>
           <a
             href={`https://github.com/${userData.githubUsername}`}
-            className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center"
+            className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center text-gray-600"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
